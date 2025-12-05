8730c12f81d0e81f5493d3505330f5395743c430
Tsjippy <ewaldha@hotmail.com>
2025-12-05 08:44:11 +0100

    fixed: enqueue module bug

--- php/enqueue.php
+++ php/enqueue.php
@@ -6,5 +6,5 @@ add_action( 'wp_enqueue_scripts', __NAMESPACE__.'\registerPDFScripts');
 
 function registerPDFScripts(){
    wp_enqueue_script_module('pdf', '//mozilla.github.io/pdf.js/build/pdf.mjs');
-   wp_enqueue_script_module('sim_pdf_script', SIM\pathToUrl(MODULE_PATH.'js/pdf.min.js'), array('pdf'), MODULE_VERSION, true);
+   wp_enqueue_script_module('sim_pdf_script', SIM\pathToUrl(MODULE_PATH.'js/pdf.min.js'), array('pdf'), MODULE_VERSION, ['in_footer' => true]);
 }
