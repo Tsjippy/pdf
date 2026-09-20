db6db6c87bda2421b70577f2f1e8a8d060441254
Tsjippy <ewaldha@hotmail.com>
2026-09-20 16:50:18 +0200

    changed: load minification js depending on SCRIPT_DEBUG constant

--- php/enqueue.php
+++ php/enqueue.php
@@ -13,5 +13,5 @@ add_action('wp_enqueue_scripts', __NAMESPACE__ . '\registerPDFScripts');
 function registerPDFScripts()
 {
     wp_enqueue_script_module('pdf', '//mozilla.github.io/pdf.js/build/pdf.mjs');
-    wp_enqueue_script_module('@tsjippy/pdf_script', TSJIPPY\pathToUrl(PLUGINPATH . 'js/pdf.min.js'), array('pdf'), PLUGINVERSION, ['in_footer' => true]);
+    wp_enqueue_script_module('@tsjippy/pdf_script', TSJIPPY\pathToUrl(PLUGINPATH . 'js/pdf' . TSJIPPY\JSEXTENSION), array('pdf'), PLUGINVERSION, ['in_footer' => true]);
 }
